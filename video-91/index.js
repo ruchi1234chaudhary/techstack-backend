3f582de79aadaf0eb3acca6d8c135bccde7b6096
Ruchi Chaudhary <cruchi971@gmail.com>
2024-03-12 22:32:57 +0530

    Add files via upload

---
+++ video-91/index.js
@@ -0,0 +1,14 @@
+//you have to write a Node.js program to clear clutter inside of a directory and organize the contents of that directory into different folders 
+
+// for example, these files become:
+// 1. name.jpg
+// 2. name.png
+// 3. this.pdf
+// 4. harry.zip
+// 5. rohan.zip
+// 6. cat.jpg
+// 7. harry.pdf
+// //this:
+// jpg/name.jpg, jpg/cat.jpg
+// png/name.png
+// zip/harry.zip, zip/rohan.zip
